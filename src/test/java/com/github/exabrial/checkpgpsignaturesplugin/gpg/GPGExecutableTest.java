9b7b298237fb30c60840b5172c5353f1ff1a1f77
Jonathan S. Fisher <exabrial@gmail.com>
2018-07-10 16:19:42 -0500

    Gpg executer test. Use powermock to mock static methods

--- src/test/java/com/github/exabrial/checkpgpsignaturesplugin/gpg/GPGExecutableTest.java
+++ src/test/java/com/github/exabrial/checkpgpsignaturesplugin/gpg/GPGExecutableTest.java
@@ -17,31 +17,73 @@
 package com.github.exabrial.checkpgpsignaturesplugin.gpg;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import javax.inject.Provider;
 
 import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.util.Os;
+import org.codehaus.plexus.util.cli.Commandline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+import com.github.exabrial.checkpgpsignaturesplugin.model.CantFindGPGException;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Os.class)
 public class GPGExecutableTest {
 	@InjectMocks
 	private GPGExecutable gpgExecutable;
+	@Mock
+	private CommandExecutor commandExecutor;
 	@Mock
 	private Logger logger;
 	@Mock
 	private Provider<String> gpgExecutableProvider;
 
 	@Test
-	public void testPostConstruct() {
-		final String yeeyee = "YEE YEE - Earl Dibbles Jr";
-		when(gpgExecutableProvider.get()).thenReturn(yeeyee);
+	public void testPostConstruct_unix() {
+		final String whichCommandOutput = "woot2";
+		final ExecutionResult result = new ExecutionResult(0, whichCommandOutput);
+		PowerMockito.spy(Os.class);
+		when(commandExecutor.execute(any(Commandline.class))).thenReturn(result);
+		PowerMockito.when(Os.isFamily(Os.FAMILY_WINDOWS)).thenReturn(false);
 		gpgExecutable.postConstruct();
-		assertEquals(yeeyee, gpgExecutable.getGPGExecutable());
+		assertEquals(whichCommandOutput, gpgExecutable.getGPGExecutable());
+	}
+
+	@Test
+	public void testPostConstruct_windows() {
+		final String whichCommandOutput = "woot3";
+		final ExecutionResult result = new ExecutionResult(0, whichCommandOutput);
+		PowerMockito.spy(Os.class);
+		when(commandExecutor.execute(any(Commandline.class))).thenReturn(result);
+		PowerMockito.when(Os.isFamily(Os.FAMILY_WINDOWS)).thenReturn(true);
+		gpgExecutable.postConstruct();
+		assertEquals(whichCommandOutput, gpgExecutable.getGPGExecutable());
+	}
+
+	@Test
+	public void testPostConstruct_provideGpgExecutable() {
+		final String gpgExectuable = "YEE YEE - Earl Dibbles Jr";
+		when(gpgExecutableProvider.get()).thenReturn(gpgExectuable);
+		gpgExecutable.postConstruct();
+		verifyZeroInteractions(commandExecutor);
+		assertEquals(gpgExectuable, gpgExecutable.getGPGExecutable());
+	}
+
+	@Test(expected = CantFindGPGException.class)
+	public void testPostConstruct_missingGpg() {
+		final String whichCommandOutput = "";
+		final ExecutionResult result = new ExecutionResult(1, whichCommandOutput);
+		when(commandExecutor.execute(any(Commandline.class))).thenReturn(result);
+		gpgExecutable.postConstruct();
 	}
 }
